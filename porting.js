23969ed65e0dc69b9ed9ae0984e0cdad2ae981d4
Tempy <usedtemplate@gmail.com>
2025-09-09 09:43:45 +0200

    Update data source and add height field in porting.js
    Changed the data source in porting.js from Data.json to OriginalDataset.json and added extraction of the height field. Updated .gitignore to exclude OriginalDataset.json.

--- porting.js
+++ porting.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const originalData = require("./Data.json");
+const originalData = require("./OriginalDataset.json");
 
 // Start benchmark
 console.time("Execution Time");
@@ -15,6 +15,7 @@ for (let i = 0; i < originalData.data.length; i++) {
     newUserData.age = user.age_1a_q_1 ?? null
     newUserData.gender = user.gender_T1_M ?? null
     newUserData.weight = user.bodyweight_kg_all_m_1_T1_M ?? null
+    newUserData.height = user.bodylength_cm_all_m_1_T1_M ?? null
     newUserData.depression = user.depression_followup_adu_q_1_T3_Q1 ?? null
     newUserData.burnout = user.burnout_followup_adu_q_1_T3_Q1 ?? null
     newUserData.alchohol_intake = user.alcohol_intake_adu_c_1_T1_QF ?? null
